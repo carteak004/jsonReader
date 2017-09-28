a7a92bc663a5607e354e1dc160c0b22acd5b2199
Kartheek Chintalapati <carteak004@gmail.com>
2017-09-28 15:36:13 -0500

    written code for XML parsing and threading

--- jsonReader/ViewController.swift
+++ jsonReader/ViewController.swift
@@ -12,26 +12,40 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, XMLParserDelegate {
 
     var personData = [person]() //instantiate a person
+    var singlePerson = person()
+    var currentElement:String = ""
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        let queueX = DispatchQueue(label: "edu.niu.cs.queueX")
         switch segmentedControl.selectedSegmentIndex
         {
-        case 0:
-            fetchJsonData()
         case 1:
+            personData = [person]()
+            fetchJsonData()
+            //DispatchQueue.main.async {
+              //  self.tableView.reloadData()
+            //}
+            
+        case 0:
+            personData = [person]()
+            queueX.sync {
             fetchXmlData()
+            }
         default:
-            break
+            queueX.sync {
+                //fetchJsonData()
+                fetchXmlData()
+            }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchJsonData()
-
+        //fetchJsonData()
+        //fetchXmlData()
         // Do any additional setup after loading the view.
     }
 
@@ -43,7 +57,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: - User defined functions
     
     func fetchJsonData() {
-        //fetching fox news latest articles
+        
+        
+        //fetching Json data
         let api_url = URL(string: "http://faculty.cs.niu.edu/%7Ekrush/ios/client_list_json.txt")
         
         //create a URL request with the API address
@@ -68,11 +84,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                         for item in clientssJson {
                             if let name = item["name"] as? String, let profession = item["profession"] as? String, let dob = item["dob"] as? String, let children = item["children"] as? [String] {
                                 
-                                print("*****MARK: BEGIN*****")
-                                print(name, profession, dob, children)
-                                print("*****MARK: END*****")
+                                self.singlePerson.name = name
+                                self.singlePerson.profession = profession
+                                self.singlePerson.dob = dob
+                                self.singlePerson.children = children
                                 
-                                self.personData.append(person(name: name, profession: profession, dob: dob, children: children))
+                                self.personData.append(self.singlePerson)
+                                
+                                print("\(self.singlePerson.name),\(self.singlePerson.profession), \(self.singlePerson.dob), \(self.singlePerson.children)")
                                 
                             }//end if
                             
@@ -81,7 +100,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     }//end if
                     
                     //if you are using a table view, you would reload the data
-                    self.tableView.reloadData()
+                    //self.tableView.reloadData()
                     
                 }//end do
                     
@@ -100,65 +119,53 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     /******************************************************************************************************/
     func fetchXmlData()
     {
-        //fetching fox news latest articles
-        let xml_url = URL(string: "http://faculty.cs.niu.edu/%7Ekrush/ios/client_list_xml.txt")
+        //personData = [person]()
         
-        //create a URL request with the API address
-        let urlRequest = URLRequest(url: xml_url!)
-        
-        //submit a request to the Json data
-        let task = URLSession.shared.dataTask(with: urlRequest) {
-            (data,response,error) in
-            //if there is an error, print it and do not continue
-            if error != nil {
-                print(error!)
-                return
-            }//end if
-            
-            //if there is no error, fetch json formatted content
-            if let content = data {
-                do {
-                    let parser = XMLParser(data: content) as AnyObject
-                    //let jsonObject = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                    
-                    //Fetch only the articles
-                    if let clientssJson = parser["clients"] as? [[String:AnyObject]] {
-                        for item in clientssJson {
-                            if let name = item["name"] as? String, let profession = item["profession"] as? String, let dob = item["dob"] as? String, let children = item["children"] as? [String] {
-                                
-                                print("*****MARK: BEGIN*****")
-                                print(name, profession, dob, children)
-                                print("*****MARK: END*****")
-                                
-                                self.personData.append(person(name: name, profession: profession, dob: dob, children: children))
-                                
-                            }//end if
-                            
-                        }//end for loop
-                        
-                    }//end if
-                    
-                    //if you are using a table view, you would reload the data
-                    self.tableView.reloadData()
-                    
-                }//end do
-                    
-                catch {
-                    
-                    print(error)
-                    
-                }//end catch
-                
-            }//end if
-            
-        }//end getdatasession
-        
-        task.resume()
-        
-        
-        
+        let url:String = "http://faculty.cs.niu.edu/%7Ekrush/ios/client_list_xml.txt"
+        let urlToSend: URL = URL(string: url)!
+        // Parse the XML
+        let parser = XMLParser(contentsOf: urlToSend)!
+        parser.delegate = self
+        parser.parse()
+        tableView.reloadData()
     }//end func
     
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        currentElement=elementName;
+        
+        if(elementName=="person")
+        {
+            if let name = attributeDict["name"], let dob = attributeDict["dob"], let profession = attributeDict["profession"]
+            {
+                singlePerson.name = name
+                singlePerson.dob = dob
+                singlePerson.profession = profession
+            }
+        }
+        
+        if(elementName=="child")
+        {
+            if let name = attributeDict["name"]
+            {
+                singlePerson.children.append(name)
+            }
+        }
+        
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        currentElement="";
+        
+        if(elementName=="person")
+        {
+            personData.append(singlePerson)
+            singlePerson = person()
+        }
+    }
+    
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print("failure error: ", parseError)
+    }
     
     /******************************************************************************************************/
     // MARK: - Table view data source
--- jsonReader/person.swift
+++ jsonReader/person.swift
@@ -9,17 +9,9 @@
 import UIKit
 
 class person: NSObject {
-    var name: String!
-    var profession: String!
-    var dob: String!
-    var children: [String]!
-    
-    init(name: String, profession: String, dob: String, children: [String])
-    {
-        self.name = name
-        self.profession = profession
-        self.dob = dob
-        self.children = children
-    }
+    var name: String = ""
+    var profession: String = ""
+    var dob: String = ""
+    var children: [String] = []
 
 }
